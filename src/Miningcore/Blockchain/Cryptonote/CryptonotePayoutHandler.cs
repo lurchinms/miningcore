4b123e0c5a3d1d26cee2817c1da4b46f9dc2bc45
Decho Kocharin <sourcedecho@outlook.com>
2025-02-26 14:33:37 +0700

    Added  NodeVersion for cryptonote family

--- src/Miningcore/Blockchain/Cryptonote/CryptonotePayoutHandler.cs
+++ src/Miningcore/Blockchain/Cryptonote/CryptonotePayoutHandler.cs
@@ -52,7 +52,6 @@ public class CryptonotePayoutHandler : PayoutHandlerBase,
     private CryptonoteNetworkType? networkType;
     private CryptonotePoolPaymentProcessingConfigExtra extraConfig;
     private bool walletSupportsTransferSplit;
-    private int PayoutMinBlockConfirmations;
 	
 
     protected override string LogCategory => "Cryptonote Payout Handler";
@@ -383,8 +382,6 @@ public class CryptonotePayoutHandler : PayoutHandlerBase,
         clusterConfig = cc;
         extraConfig = pc.PaymentProcessing.Extra.SafeExtensionDataAs<CryptonotePoolPaymentProcessingConfigExtra>();
 
-        var coin = poolConfig.Template.As<CryptonoteCoinTemplate>();
-        PayoutMinBlockConfirmations = coin?.CoinbaseMinConfimations ?? CryptonoteConstants.PayoutMinBlockConfirmations;
         logger = LogUtil.GetPoolScopedLogger(typeof(CryptonotePayoutHandler), pc);
 
         // configure standard daemon
@@ -470,6 +467,7 @@ public class CryptonotePayoutHandler : PayoutHandlerBase,
                 var blockHeader = rpcResult.Response.BlockHeader;
 
                 // update progress
+				int PayoutMinBlockConfirmations = coin?.CoinbaseMinConfimations ?? CryptonoteConstants.PayoutMinBlockConfirmations;
                 block.ConfirmationProgress = Math.Min(1.0d, (double) blockHeader.Depth / PayoutMinBlockConfirmations);
                 result.Add(block);
 
@@ -518,7 +516,6 @@ public class CryptonotePayoutHandler : PayoutHandlerBase,
 
                         case "XEQ":
                             decimal EquilibriaMiningReward = EquilibriaConstants.EquilibriaMiningRewardInitial;
-                            decimal EquilibriaReserveReward = EquilibriaConstants.EquilibriaReserveRewardInitial;
                             
                             block.Reward = (((blockHeader.Reward / coin.SmallestUnit)) * EquilibriaMiningReward) * coin.BlockrewardMultiplier;
                             break;
