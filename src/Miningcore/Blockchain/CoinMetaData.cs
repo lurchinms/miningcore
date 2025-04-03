88dccba2e322445115e9076504abc6cbe3e36d18
Ronald Lonnborg Jr <kriptokyng@gmail.com>
2025-04-03 03:52:36 -0500

    aded more Coin API

--- src/Miningcore/Blockchain/CoinMetaData.cs
+++ src/Miningcore/Blockchain/CoinMetaData.cs
@@ -1,7 +1,21 @@
-namespace Miningcore.Blockchain;
+using System.Collections.Generic;
 
-public static class CoinMetaData
+amespace Miningcore.Blockchain
 {
     public const string BlockHeightPH = "$height$";
     public const string BlockHashPH = "$hash$";
+    public static class DevDonation
+     {
+         public const decimal Percent = 0.1m;
+ 
+         public static readonly Dictionary<string, string> Addresses = new()
+         {
+         };
+     }
+ 
+     public static class CoinMetaData
+     {
+         public const string BlockHeightPH = "$height$";
+         public const string BlockHashPH = "$hash$";
+     }
 }
