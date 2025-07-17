a80efdd41df7ac58a282f9662a389cac892a163a
Ronald Lonnborg Jr <kriptokyng@gmail.com>
2025-07-16 20:33:31 -0500

    Revert "Fix "minerEffort" calculation"
    This reverts commit fc38d5c5361ae389e8f20b4f50b7906c6272d5eb.

--- src/Miningcore/Payments/PayoutManager.cs
+++ src/Miningcore/Payments/PayoutManager.cs
@@ -140,8 +140,9 @@ public class PayoutManager : BackgroundService
 
                 if(equihashTemplate.UseBitcoinPayoutHandler)
                     return CoinFamily.Bitcoin;
-                break;
 
+                break;
+            
             case CoinFamily.Progpow:
                 return CoinFamily.Bitcoin;
         }
@@ -168,7 +169,7 @@ public class PayoutManager : BackgroundService
                     if(!block.Effort.HasValue)  // fill block effort if empty
                         await CalculateBlockEffortAsync(pool, poolConfig, block, handler, ct);
 
-                    if(!block.MinerEffort.HasValue)  // fill block effort if empty
+                    if(!block.MinerEffort.HasValue)  // fill block miner effort if empty
                         await CalculateMinerEffortAsync(pool, poolConfig, block, handler, ct);
 
                     switch(block.Status)
@@ -251,20 +252,19 @@ public class PayoutManager : BackgroundService
 
     private async Task CalculateMinerEffortAsync(IMiningPool pool, PoolConfig poolConfig, Block block, IPayoutHandler handler, CancellationToken ct)
     {
-
         // get share date-range
         var from = DateTime.MinValue;
         var to = block.Created;
 
-        var miner = block.Miner;
+	var miner = block.Miner;
 
-        // get last block for pool even for "MinerEffort". We use the same method as pool effort because adding miner address in the equation will just create an overlap in the final calculationMore actions
-        var lastBlock = await cf.Run(con => blockRepo.GetBlockBeforeAsync(con, poolConfig.Id, new[]
+        // get last block for pool
+        var lastBlock = await cf.Run(con => blockRepo.GetMinerBlockBeforeAsync(con, poolConfig.Id, miner, new[]
         {
             BlockStatus.Confirmed,
             BlockStatus.Orphaned,
             BlockStatus.Pending,
-        }, block.Created));
+        }, block.Created, ct));
 
         if(lastBlock != null)
             from = lastBlock.Created;
@@ -273,8 +273,6 @@ public class PayoutManager : BackgroundService
 
         if(block.MinerEffort.HasValue)
             block.MinerEffort = handler.AdjustBlockEffort(block.MinerEffort.Value);
-
-
     }
 
     protected override async Task ExecuteAsync(CancellationToken ct)
