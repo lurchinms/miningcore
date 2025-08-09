1f6c5966430a658f1035266f5118acf6dc92f4cc
Ronald Lonnborg Jr <kriptokyng@gmail.com>
2025-08-09 18:38:40 -0500

    add YespowerSugar

--- src/Miningcore/Native/Multihash.cs
+++ src/Miningcore/Native/Multihash.cs
@@ -206,6 +206,8 @@ public static unsafe class Multihash
     public static extern void yespowerR16(byte* input, void* output, uint inputLength);
 
     [DllImport("libmultihash", EntryPoint = "yespowerTIDE_export", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void yespowerSUGAR(byte* input, void* output, uint inputLength);
+    [DllImport("libmultihash", EntryPoint = "yespowerSUGAR_export", CallingConvention = CallingConvention.Cdecl)]
     public static extern void yespowerTIDE(byte* input, void* output, uint inputLength);
 
     [DllImport("libmultihash", EntryPoint = "flex_export", CallingConvention = CallingConvention.Cdecl)]
