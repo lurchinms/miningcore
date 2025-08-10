be1d175a9f2db09adc45b2dcec5b361e1b4a78fa
Ronald Lonnborg Jr <kriptokyng@gmail.com>
2025-08-09 19:52:35 -0500

    fix yespower

--- src/Miningcore/Native/Multihash.cs
+++ src/Miningcore/Native/Multihash.cs
@@ -196,6 +196,7 @@ public static unsafe class Multihash
     [DllImport("libmultihash", EntryPoint = "power2b_export", CallingConvention = CallingConvention.Cdecl)]
     public static extern void power2b(byte* input, void* output, uint inputLength);
 
+    [DllImport("libmultihash", EntryPoint = "yespower_export", CallingConvention = CallingConvention.Cdecl)]
     public static extern void yespower(byte* input, void* output, uint inputLength);
 
     [DllImport("libmultihash", EntryPoint = "yespowerADVC_export", CallingConvention = CallingConvention.Cdecl)]
